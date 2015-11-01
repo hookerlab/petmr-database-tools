58fddfb3f3f820af51b3d0b223cecc6b252fde08
jphan <strike127@users.noreply.github.com>
2015-11-01 01:53:37 -0400

    Update RpLog.py
    added unrelated data comment mover/swap
    tidied up file

--- RpLog.py
+++ RpLog.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import json
+import csv
 __author__ = 'jphan'
 
 #read in excel file
@@ -7,57 +9,64 @@ data = pd.read_csv("RADIOPHARM log - Summary-mod.csv")
 #get number of rows
 num_rows = len(data)
 
+#holds list of indexes with time formatting errors
+errList = []
+
 #title row
 print("Total rows: {}".format(num_rows))
 print(list(data))
 
-#rename first column title to Date
-data.rename(columns={' ': 'Date'}, inplace=True)
-print(list(data))
-
-#drop unneeded column
-data2 = data.drop(['STUDY', 'PI'], axis=1)
-
 #renamed Columns
-data2.rename(columns={'Unnamed: 17': 'Comments'}, inplace=True)
-data2.rename(columns={'Time': 'Initial_Time'}, inplace=True)
-data2.rename(columns={'Time.1': 'Residual_Time'}, inplace=True)
-data2.rename(columns={'Scan time/\nInjection time': 'TOI'}, inplace=True)
-# data2.rename(columns={'Unnamed: 34': 'Indicies_With_Error'}, inplace=True)
-print(list(data2))
+data.rename(columns={' ': 'Date'}, inplace=True)
+data.rename(columns={'Initial \n(mCi)': 'Initial (mCi)'}, inplace=True)
+data.rename(columns={'Time': 'Initial Time'}, inplace=True)
+data.rename(columns={'Residual\n(mCi)': 'Residual (mCi)'}, inplace=True)
+data.rename(columns={'Time.1': 'Residual Time'}, inplace=True)
+data.rename(columns={'Scan time/\nInjection time': 'Injection Time'}, inplace=True)
+data.rename(columns={'Used\n(mCi)': 'Used (mCi)'}, inplace=True)
+data.rename(columns={'Tracer\nAdmin': 'Tracer Admin'}, inplace=True)
+data.rename(columns={'C11/F18\nproduction': 'C11/F18 Production'}, inplace=True)
+data.rename(columns={'Image\nAnalysis': 'Image Analysis'}, inplace=True)
+data.rename(columns={'Blood\nAnalysis': 'Blood Analysis'}, inplace=True)
+data.rename(columns={'Unnamed: 17': 'Comments'}, inplace=True)
 
 #changes data in column to lowercase
-data2['SUBJECT'] = data2['SUBJECT'].str.lower()
-data2['MANUF.'] = data2['MANUF.'].str.lower()
-data2['Blood\nAnalysis'] = data2['Blood\nAnalysis'].str.lower()
-data2['Image\nAnalysis'] = data2['Image\nAnalysis'].str.lower()
-data2['C11/F18\nproduction'] = data2['C11/F18\nproduction'].str.lower()
+data['SUBJECT'] = data['SUBJECT'].str.lower()
+data['MANUF.'] = data['MANUF.'].str.lower()
+data['Blood Analysis'] = data['Blood Analysis'].str.lower()
+data['Image Analysis'] = data['Image Analysis'].str.lower()
+data['C11/F18 Production'] = data['C11/F18 Production'].str.lower()
+data['Initial (mCi)'] = data['Initial (mCi)'].str.lower()
+data['Residual (mCi)'] = data['Residual (mCi)'].str.lower()
 
 #fills in empty cells
-data2.fillna(value = 'NULL',inplace=True)
+data.fillna(value='NULL', inplace=True)
 
-#change yes/no to boolean 1/0                         PROBLEM RETURNS A DOUBLE INSTEAD OF INT IN CONSOLE BUT NOT EXCEL??
+#checks if subject is one of the 4 options
+bo = True
+for dat in data['SUBJECT']:
+    if dat == 'human' or dat == 'animal' or dat == 'phantom' or dat == 'NULL':
+        bo = True
+    else:
+        bo = False
+        break
+
+#change yes/no to boolean 1/0
 aboolean = {'yes': 1, 'no': 0}
-data2['Blood\nAnalysis'] = data2['Blood\nAnalysis'].map(aboolean)
-
-data2['Image\nAnalysis'] = data2['Image\nAnalysis'].map(aboolean)
-
-data2['C11/F18\nproduction'] = data2['C11/F18\nproduction'].map(aboolean)
+data['Blood Analysis'] = data['Blood Analysis'].map(aboolean)
+data['Image Analysis'] = data['Image Analysis'].map(aboolean)
+data['C11/F18 Production'] = data['C11/F18 Production'].map(aboolean)
 
 #converts date mm/dd/yyyy to yyyy-mm-dd
 def dateconv(date):
-    dt = pd.datetime.strptime(data2['Date'][date], '%m/%d/%Y')
+    dt = pd.datetime.strptime(data['Date'][date], '%m/%d/%Y')
     return str('{0}-{1}-{2}'.format(dt.year, dt.month, dt.day % 100))
 
 #converts all the data dates
 x = 0
 while x < num_rows:
-    data2.ix[x, 'Date'] = dateconv(x)
+    data.ix[x, 'Date'] = dateconv(x)
     x += 1
-
-#convert 12 hr to 24 hr
-def timeconv(colName, time):
-    return str(pd.datetime.strptime(data2[colName][time], '%I:%M:%S %p').time())
 
 #checks if time is already in 24hr format
 def isTimeFormat(input):
@@ -67,17 +76,19 @@ def isTimeFormat(input):
     except ValueError:
         return False
 
-#holds list of indexes with time formatting errors
-errList = []
+#convert 12 hr to 24 hr
+def timeconv(colName, time):
+    return str(pd.datetime.strptime(data[colName][time], '%I:%M:%S %p').time())
+
+#converts all time data to 24hr format, indexes of times with errors are saved to errList
 def allTimeConv(colName):
-    #converts all time data to 24hr format, indexes of times with errors are saved to errList
     y = 0
     while y < num_rows:
-        curr = data2[colName][y]
+        curr = data[colName][y]
         timecheck = isTimeFormat(curr)
-        if not(curr == 'NULL' or curr == 'N/A' or curr == 'n/a' or curr == 'N/a' or curr == 'n/A' or timecheck == True):#ON THIS PART FIND HOW TO FIX ROW 47 already in mil time
+        if not(curr == 'NULL' or curr == 'N/A' or curr == 'n/a' or curr == 'N/a' or curr == 'n/A' or timecheck == True):
             try:
-                data2.ix[y, colName] = timeconv(colName, y)
+                data.ix[y, colName] = timeconv(colName, y)
             except ValueError:
                 # print(y)
                 if y not in errList:
@@ -87,16 +98,15 @@ def allTimeConv(colName):
             y += 1
 
 #converts all time data for initial,residual, and TOI
-allTimeConv('Initial_Time')
-allTimeConv('Residual_Time')
-allTimeConv('TOI')
-errList.sort()
+allTimeConv('Initial Time')
+allTimeConv('Residual Time')
+allTimeConv('Injection Time')
 
 #gets unique values in a column
-list_sub = pd.unique(data2['SUBJECT'])
-list_man = pd.unique(data2['MANUF.'])
-list_int = pd.unique(data2['Initial (mCi)'])
-list_res = pd.unique(data2['Residual (mCi)'])
+list_sub = pd.unique(data['SUBJECT'])
+list_man = pd.unique(data['MANUF.'])
+list_int = pd.unique(data['Initial (mCi)'])
+list_res = pd.unique(data['Residual (mCi)'])
 
 #returns a list of unique non number entries
 def findUni(inp):
@@ -110,7 +120,7 @@ def findUni(inp):
                     tempList.append(x)
                     break
     return tempList
-    
+
 #finds unique non number and sorts
 uni_man = findUni(list_man)
 uni_man.sort()
@@ -119,34 +129,46 @@ uni_int.sort()
 uni_res = findUni(list_res)
 uni_res.sort()
 
-#relabels common manufactures to a common name
-for x in data2['MANUF.']:
-    if 'cardinal' in x:
-        data2.ix[x, 'MANUF.'] = 'cardinal health'
-    elif 'iba' or 'molec' in x:
-        data2.ix[x, 'MANUF.'] = 'i.b.a. molec'
-    elif 'house' in x:
-        data2.ix[x, 'MANUF.'] = 'in house'
-
-#swaps unrealated data to comments
+#swaps unrelated data to comments
 def commInit(colName, count):
-    if data2['Comments'][count] == 'NULL':
-            data2.ix[count, 'Comments'] = data2[colName][count]
-            data2.ix[count, colName] = 'NULL'
+    if data['Comments'][count] == 'NULL':
+            data.ix[count, 'Comments'] = data[colName][count]
+            data.ix[count, colName] = 'NULL'
     else:
-            data2.ix[count, 'Comments'] += '; ' + data2[colName][count]
-            data2.ix[count, colName] = 'NULL'
+            data.ix[count, 'Comments'] += '; ' + data[colName][count]
+            data.ix[count, colName] = 'NULL'
 
+#moves random data to comments column
+def commMove(colName, uni_list):
+    counter = 0
+    for x in data[colName]:
+        for y in uni_list:
+            if y in x:
+                commInit(colName, counter)
 
+        counter += 1
 
+#moves unrelated non number data to comments section
+commMove('Initial (mCi)', uni_int)
+commMove('Residual (mCi)', uni_res)
 
+#relabels common manufactures to a common name
+counts = 0
+for x in data['MANUF.']:
+    if 'cardinal' in x:
+        data.ix[counts, 'MANUF.'] = 'cardinal health'
+    elif 'iba' in x or 'molec.' in x:
+        data.ix[counts, 'MANUF.'] = 'i.b.a. molec'
+    elif 'house' in x:
+        data.ix[counts, 'MANUF.'] = 'in house'
+    counts += 1
 
+#saves to csv file
+writefile = pd.DataFrame(data)
+writefile.to_csv('final_edited.csv', index=False, na_rep='null')
 
-#save a csv file with a list of indexes with errors        
+#saves csv file of indexes with data error
+# errList.sort()
 # errIndex = pd.DataFrame(errList)
 # errIndex.columns = ['Error_Indexes']
-# errIndex.to_csv('Indexes_With_Errors.csv', index=False, na_rep = 'null' )
-
-#saves a csv file the data
-# test = pd.DataFrame(data2)
-# test.to_csv('test2.csv', index=False, na_rep = 'null' )
+# errIndex.to_csv('Indexes_With_Errors.csv', index=False, na_rep='null')
