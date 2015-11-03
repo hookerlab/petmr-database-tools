55d661ca1ebd2d2e8aae495208f7360b8aeff851
jphan <strike127@users.noreply.github.com>
2015-11-03 18:44:01 -0500

    Update RpLog.py
    added docstrings

--- RpLog.py
+++ RpLog.py
@@ -1,26 +1,26 @@
 import pandas as pd
 import json
-import csv
-import sys
+
 __author__ = 'jphan'
 
 
-# read in excel file
+# reads in excel file path
 filename = input('Enter Path to file')
 data = pd.read_csv(filename)
 # data = pd.read_csv("RADIOPHARM log - Summary.csv")
+#C:\Users\strike\Desktop\project\RADIOPHARM log - Summary.csv
 
-# get number of rows
+# gets the number of rows
 num_rows = len(data)
 
-# holds list of indexes with time formatting errors
+# holds a list of indexes with time formatting errors
 errList = []
 
 # title row
 print("Total rows: {}".format(num_rows))
 print(list(data))
 
-# renamed Columns
+# renaming Columns
 data.rename(columns={' ': 'Date'}, inplace=True)
 data.rename(columns={'Initial \n(mCi)': 'Initial (mCi)'}, inplace=True)
 data.rename(columns={'Time': 'Initial Time'}, inplace=True)
@@ -35,7 +35,7 @@ data.rename(columns={'Blood\nAnalysis': 'Blood Analysis'}, inplace=True)
 data.rename(columns={'Unnamed: 17': 'Comments'}, inplace=True)
 print(list(data))
 
-# changes data in column to lowercase
+# changes all data in column to lowercase
 data['SUBJECT'] = data['SUBJECT'].str.lower()
 data['MANUF.'] = data['MANUF.'].str.lower()
 data['Blood Analysis'] = data['Blood Analysis'].str.lower()
@@ -44,7 +44,7 @@ data['C11/F18 Production'] = data['C11/F18 Production'].str.lower()
 data['Initial (mCi)'] = data['Initial (mCi)'].str.lower()
 data['Residual (mCi)'] = data['Residual (mCi)'].str.lower()
 
-# fills in empty cells
+# fills in empty cells or variations of n/a with 'NULL'
 data.fillna(value='NULL', inplace=True)
 temp = 0
 nalist = ['n/a', 'N/A', 'N/a', 'n/A']
@@ -56,17 +56,24 @@ while temp < num_rows:
                 break
     temp += 1
 
-# checks if subject is one of the 4 options
-bo = True
-for dat in data['SUBJECT']:
-    if dat == 'human' or dat == 'animal' or dat == 'phantom' or dat == 'NULL':
-        bo = True
-    else:
-        bo = False
-        break
+# checks if subject is one of the 4 options, used for debugging
+# bo = True
+# for dat in data['SUBJECT']:
+#     if dat == 'human' or dat == 'animal' or dat == 'phantom' or dat == 'NULL':
+#         bo = True
+#     else:
+#         bo = False
+#         break
 
-# converts yes/no to 1/0
 def boolConv(col):
+    """ converts yes/no to 1/0
+    Parameters:
+    col: String
+        Name of column
+    Return:
+    Out: Object
+        Returns each row of the column as an object because there are cells that are 'NULL' from missing data
+    """
     counts = 0
     for x in data[col]:
         if 'yes' in x:
@@ -75,12 +82,20 @@ def boolConv(col):
             data.ix[counts, col] = int(0)
         counts += 1
 
+#Calls boolConv function to convert yes/no to 1/0 according to the column
 boolConv('Blood Analysis')
 boolConv('Image Analysis')
 boolConv('C11/F18 Production')
 
-# converts date mm/dd/yyyy to yyyy-mm-dd
 def dateconv(date):
+    """ converts date mm/dd/yyyy to yyyy-mm-dd
+    Parameters:
+    date: String
+        String of date of the current row in the 'Date' column
+    Returns:
+    Out: String
+        Returns reformatted date yyyy-mm-dd
+    """
     formtype = ['%m/%d/%y', '%m/%d/%Y']
     for f in formtype:
         try:
@@ -90,7 +105,7 @@ def dateconv(date):
             pass
     return str('{0}-{1}-{2}'.format(dt.year, dt.month, dt.day % 100))
 
-# converts all the data dates
+# Reformats all the rows in dates to yyyy-mm-dd
 x = 0
 while x < num_rows:
     try:
@@ -101,20 +116,44 @@ while x < num_rows:
             errList.append(x)
     x += 1
 
-# checks if time is already in 24hr format
 def isTimeFormat(input):
+    """ checks if time is already in 24hr format
+    Parameter:
+    input: String
+        String of the time input
+    Returns:
+    Out: Boolean
+        True: if time string is already in 24hr format
+        False: if time string is not in 24hr format
+    """
     try:
         pd.datetime.strptime(input, '%H:%M:%S').time()
         return True
     except ValueError:
         return False
 
-# convert 12 hr to 24 hr
-def timeconv(colName, time):
-    return str(pd.datetime.strptime(data[colName][time], '%I:%M:%S %p').time())
+def timeconv(colName, row):
+    """ convert 12 hr to 24 hr
+    Parameters:
+    colName: String
+        Name of column
+    row: String
+        Current row in the column
+    Return:
+    Out: String
+        Returns the time in 24hr format
+    """
+    return str(pd.datetime.strptime(data[colName][row], '%I:%M:%S %p').time())
 
-# converts all time data to 24hr format, indexes of times with errors are saved to errList
 def allTimeConv(colName):
+    """ converts all time data to 24hr format, indexes of times with errors are saved to errList
+    Parameter:
+    colName: String
+        Name of column
+    See Also:
+    isTimeFormat(): checks if time is already in 24hr format
+    timeconv(): converts time to 24hr format
+    """
     y = 0
     while y < num_rows:
         curr = data[colName][y]
@@ -130,19 +169,26 @@ def allTimeConv(colName):
         else:
             y += 1
 
-# converts all time data for initial,residual, and TOI
+# Calls allTimeConv to convert all time data for initial,residual, and TOI to 24hr format
 allTimeConv('Initial Time')
 allTimeConv('Residual Time')
 allTimeConv('Injection Time')
 
-# gets unique values in a column
+# gets the unique values in a column
 list_sub = pd.unique(data['SUBJECT'])
 list_man = pd.unique(data['MANUF.'])
 list_int = pd.unique(data['Initial (mCi)'])
 list_res = pd.unique(data['Residual (mCi)'])
 
-# returns a list of unique non number entries
 def findUni(inp):
+    """ returns a list of unique non-number value
+    Parameter:
+    inp: Object
+        List of unique values
+    Return:
+    Out: Object
+        Returns a list of unique non-number values
+    """
     tempList = []
     alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
              'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -154,7 +200,7 @@ def findUni(inp):
                     break
     return tempList
 
-# finds unique non number and sorts
+# finds unique non-number values and sorts the lists
 uni_man = findUni(list_man)
 uni_man.sort()
 uni_int = findUni(list_int)
@@ -162,7 +208,7 @@ uni_int.sort()
 uni_res = findUni(list_res)
 uni_res.sort()
 
-# concatenates the extra comments
+# concatenates the extra comments for columns 18 to 34
 comm_loc = data.columns.get_loc("Comments")
 temp = 0
 while temp < num_rows:
@@ -175,26 +221,40 @@ while temp < num_rows:
                 data.ix[temp, 'Comments'] = '"' + str(data[col][temp]) + '"'
     temp += 1
 
-# swaps unrelated data to comments
-def commInit(colName, count):
-    if data[colName][count] != 'NULL':
-        if data['Comments'][count] == 'NULL':
-            data.ix[count, 'Comments'] = '"' + data[colName][count] + '"'
-            data.ix[count, colName] = 'NULL'
+def commInit(colName, row):
+    """ Moves unrelated data to comments and sets original position to 'NULL'
+    Parameter:
+    colName: String
+        Name of column
+    row: int
+        Current row
+    """
+    if data[colName][row] != 'NULL':
+        if data['Comments'][row] == 'NULL':
+            data.ix[row, 'Comments'] = '"' + data[colName][row] + '"'
+            data.ix[row, colName] = 'NULL'
         else:
-            data.ix[count, 'Comments'] += ', "' + data[colName][count] + '"'
-            data.ix[count, colName] = 'NULL'
+            data.ix[row, 'Comments'] += ', "' + data[colName][row] + '"'
+            data.ix[row, colName] = 'NULL'
 
-# moves random data to comments column
 def commMove(colName, uni_list):
-    counter = 0
+    """ moves unrelated data to comments column
+    Parameter:
+    colName: String
+        Name of column
+    uni_list: Object
+        List of unique values
+    See Also:
+    commInit: moves unique value to comment and sets original cell to 'NULL'
+    """
+    row = 0
     for x in data[colName]:
         for y in uni_list:
             if y in x:
-                commInit(colName, counter)
-        counter += 1
+                commInit(colName, row)
+        row += 1
 
-# moves unrelated non number data to comments section
+# moves unrelated non-number data to comments section
 commMove('Initial (mCi)', uni_int)
 commMove('Residual (mCi)', uni_res)
 
@@ -209,7 +269,7 @@ for x in data['MANUF.']:
         data.ix[counts, 'MANUF.'] = 'in house'
     counts += 1
 
-# drop unneeded column
+# drop unneeded columns
 data = data.drop(['STUDY', 'PI'], axis=1)
 data = data.drop(data.columns[16:], axis=1)
 
@@ -218,7 +278,7 @@ writefile = pd.DataFrame(data)
 writefile.to_csv('RADIOPHARM log - Summary_edited.csv', index=False, na_rep='NULL')
 
 #creates JSON file of data
-myJSON = data.to_json(path_or_buf=None, orient='records', date_format='epoch', double_precision=10, force_ascii=True,
+myJSON = data.to_json(path_or_buf=None, orient='index', date_format='epoch', double_precision=10, force_ascii=True,
                       date_unit='ms', default_handler=None)
 print(myJSON)
 with open('RadioPharmLog.json', 'w') as outfile:
