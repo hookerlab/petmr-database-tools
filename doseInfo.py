6b10e1daecb1c9592a6b9f979fb3c3413f30e8e9
jphan <strike127@users.noreply.github.com>
2015-11-13 23:51:53 -0500

    Create doseInfo.py
    gets list of paths to all dose_info.xls files

---
+++ doseInfo.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import json
+import os
+
+__author__ = 'jphan'
+
+pathname = input('Enter Path to files')
+pathList = []
+for dirName, subdirList, fileList in os.walk(pathname):
+    print('Found directory: %s' % dirName)
+    for fname in fileList:
+        if 'Dose_info' in fname:
+            pathList.append(os.path.join(dirName, fname))
+            print('\t%s' % fname)
+print(pathList)
