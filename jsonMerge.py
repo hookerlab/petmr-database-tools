4f08a78369a3c9b89d65bc352ee216bdcba6138a
jphan <strike127@users.noreply.github.com>
2015-11-19 12:11:51 -0500

    Update jsonMerge.py
    added import
    type change of dose_info columns
    added list of dates for easy access of year for Dose_info
    added dataframe to JSON obj for both merged and dose_info_with_id

--- jsonMerge.py
+++ jsonMerge.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 from collections import OrderedDict
 import sys
+import os
 
 __author__ = 'jphan'
 '''
@@ -23,19 +24,21 @@ if total == 3:
 else:
     #get path to both JSON files
     inp_rp = input('Enter path to RadioPharmLog.json: ')
-    # inp_rp = 'C:/Users/strike/Desktop/projFiles/RadioPharmLog.json'
     inp_rp = inp_rp.strip()
     inp_dose = input('Enter path to Dose_info.json: ')
-    # inp_dose = 'C:/Users/strike/Desktop/projFiles/Dose_info.json'
     inp_dose = inp_dose.strip()
 
 #reads in json files
-data_rp = pd.read_json(inp_rp)
+data_rp = pd.read_json(inp_rp, convert_dates=False)
 data_dose = pd.read_json(inp_dose)
 
 #number of rows in each file
 num_rowsRp = len(data_rp)
 num_rowsDose = len(data_dose)
+
+#converts Dose_info columns to object to allow serializability
+for col in data_dose:
+    data_dose[col] = data_dose[col].astype(object)
 
 #Creates new 'NULL' columns in radiopharm dataframe with column names of dose_info columns
 for col in data_dose:
@@ -56,18 +59,19 @@ def dateconv(date):
     formtype = ['%Y%m%d', '%Y-%m-%d']
     for f in formtype:
         try:
-            # dt = pd.datetime.strptime(data['Date'][date], f)
             dt = pd.datetime.strptime(str(data_dose['scan date(YYYYMMDD)'][date]), f).date()
             break
         except ValueError:
             pass
-    # return str('{0}-{1}-{2}'.format(dt.year, dt.month, dt.day % 100))
     return dt
     
 #convert dose_info date to date format
 row = 0
+dose_dateList = []
 while row < num_rowsDose:
-    data_dose.ix[row, 'scan date(YYYYMMDD)'] = dateconv(row)
+    currDate = dateconv(row)
+    dose_dateList.append(currDate)
+    data_dose.ix[row, 'scan date(YYYYMMDD)'] = str(currDate)
     row += 1
     
 def loopMerge(dose_row, start, end):
@@ -81,10 +85,10 @@ def loopMerge(dose_row, start, end):
         Upper bound for range of current dose_info year
     """
     while start <= end:
-        if (str(data_dose['scan date(YYYYMMDD)'][dose_row]) == str(data_rp['Date'][start].date()) and
-                data_dose['series time'][dose_row] == data_rp['Injection Time'][start]):
+        if (str(data_dose['scan date(YYYYMMDD)'][dose_row]) == str(data_rp['Date'][start]) and
+                data_dose['toi'][dose_row] == data_rp['Injection Time'][start]):
             data_dose.ix[dose_row, 'ID'] = data_rp['ID'][start]
-            data_dose.ix[dose_row, 'Row Index'] = int(start)
+            data_dose.ix[dose_row, 'Row Index'] = int(start) + 2
             for cols in data_dose:
                 if cols != 'quant_param':
                     if cols != 'ID':
@@ -107,22 +111,57 @@ Year - Rows
 #Merge files based on Date and Time checks
 row_dose = 0
 while row_dose < num_rowsDose:
-    if data_dose['scan date(YYYYMMDD)'][row_dose].year == 2010:
+    if dose_dateList[row_dose].year == 2010:
         loopMerge(row_dose, 0, 12)
-    elif data_dose['scan date(YYYYMMDD)'][row_dose].year == 2011:
+    elif dose_dateList[row_dose].year == 2011:
         loopMerge(row_dose, 13, 55)
-    elif data_dose['scan date(YYYYMMDD)'][row_dose].year == 2012:
+    elif dose_dateList[row_dose].year == 2012:
         loopMerge(row_dose, 56, 266)
-    elif data_dose['scan date(YYYYMMDD)'][row_dose].year == 2013:
+    elif dose_dateList[row_dose].year == 2013:
         loopMerge(row_dose, 267, 575)
-    elif data_dose['scan date(YYYYMMDD)'][row_dose].year == 2014:
+    elif dose_dateList[row_dose].year == 2014:
         loopMerge(row_dose, 576, 904)
-    elif data_dose['scan date(YYYYMMDD)'][row_dose].year == 2015:
+    elif dose_dateList[row_dose].year == 2015:
         loopMerge(row_dose, 905, 1173)
     else:
         loopMerge(row_dose, 1174, (num_rowsRp - 1))
     row_dose += 1
     
+#creates dictionary to save as JSON object - Merged JSON
+rpList = []
+count = 0
+while count < num_rowsRp:
+    rp = OrderedDict()
+    for col in data_rp:
+        rp[col] = data_rp[col][count]
+    rpList.append(rp)
+    count += 1
 
+j = json.dumps(rpList)
+# print(j)
+savePath = input('Enter save directory path for Merged JSON: ')
+savePath = savePath.strip()
+completeName = os.path.join(savePath, 'merged.json')
+print('Saving: ' + completeName)
+with open(completeName, 'w') as f:
+    f.write(j)
 
+#creates dictionary to save as JSON object - dose_info with ID + row index JSON
+doseList = []
+count = 0
+while count < num_rowsDose:
+    dose = OrderedDict()
+    for col in data_dose:
+        dose[col] = data_dose[col][count]
+    doseList.append(dose)
+    count += 1
 
+k = json.dumps(doseList)
+# print(k)
+savePath = input('Enter save directory path for Dose_info with ID JSON: ')
+savePath = savePath.strip()
+completeName = os.path.join(savePath, 'Dose_info_with_ID.json')
+print('Saving: ' + completeName)
+with open(completeName, 'w') as f:
+    f.write(k)
+    
